5791794e53a9521dc7b8daafbe50b3ccfc6242c6
paulchen <paulchen@rueckgr.at>
2013-08-29 14:31:27 +0200

    update

--- web/js/main.js
+++ web/js/main.js
@@ -2,7 +2,8 @@ function initialize() {
 	var mapOptions = {
 		center: new google.maps.LatLng(48.21, 16.37),
 		zoom: 12,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		streetViewControl: false
 	};
 	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 }
