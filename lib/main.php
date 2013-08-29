83b2023d3b32492eb0b032f8136be3ae662b5b85
paulchen <paulchen@rueckgr.at>
2013-08-29 22:06:08 +0200

    update

--- lib/main.php
+++ lib/main.php
@@ -2,5 +2,15 @@
 
 require_once(dirname(__FILE__) . '/common.php');
 
+$lines = array();
+$data = db_query('SELECT id, name FROM line_type');
+foreach($data as $row) {
+	$lines[$row['id']] = array('name' => $row['name'], 'lines' => array());
+}
+$data = db_query('SELECT id, name, type FROM line ORDER BY name ASC');
+foreach($data as $row) {
+	$lines[$row['type']]['lines'][] = $row;
+}
+
 require_once("$template_dir/index.php");
 
--- templates/index.php
+++ templates/index.php
@@ -1,3 +1,14 @@
+<?php
+function show_lines($type) {
+	global $lines;
+
+	echo "<h2>" . htmlentities($lines[$type]['name'], ENT_QUOTES, 'UTF-8') . "</h2>";
+
+	foreach($lines[$type]['lines'] as $line) {
+		echo "<input type='checkbox' name='checkbox_line_{$line['id']}' onclick='toggle({$line['id']});'>&nbsp;{$line['name']}<br />";
+	}
+}
+?>
 <!DOCTYPE html>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
@@ -6,9 +17,21 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css" />
 	<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAj4Id5jnWqbSeAm0YcSoep75ujK2h8T70&sensor=false"></script>
 	<script type="text/javascript" src="js/main.js"></script>
+	<script type="text/javascript">
+	<!--
+	function toggle(id) {
+		alert(id);
+	}
+	// -->
+	</script>
 </head>
 <body onload="initialize();">
-	<div id="map_canvas" style="width:100%; height:100%"></div>
+	<div id="options_pane" style="width: 20%; height: 100%; position: absolute; overflow: scroll;">
+		<?php show_lines(4); ?>
+		<?php show_lines(1); ?>
+		<?php show_lines(2); ?>
+	</div>
+	<div id="map_canvas" style="position: absolute; width:80%; left: 20%; height:100%"></div>
 </body>
 </html>
 
