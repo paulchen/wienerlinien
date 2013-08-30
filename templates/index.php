d9a4b12433911facda7a154fa26d136611beb33b
paulchen <paulchen@rueckgr.at>
2013-08-30 15:15:06 +0200

    update

--- templates/index.php
+++ templates/index.php
@@ -16,80 +16,8 @@ function show_lines($type) {
 	<title>Wiener Linien</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css" />
 	<script type="text/javascript" src="//maps.googleapis.com/maps/api/js?key=AIzaSyAj4Id5jnWqbSeAm0YcSoep75ujK2h8T70&sensor=false"></script>
-	<script type="text/javascript" src="js/main.js"></script>
 	<script type="text/javascript" src="js/jquery.min.js"></script>
-	<script type="text/javascript">
-	<!--
-	var line_data = new Array();
-	var segments = new Array();
-	var shown = new Array();
-
-	function show_hide(id) {
-		if((id in shown) && shown[id]) {
-			hide(id);
-		}
-		else {
-			show(id);
-		}
-	}
-
-	function hide(id) {
-		$.each(segments[id], function(index, value) {
-			value.setVisible(false);
-		});
-		shown[id] = false;
-	}
-
-	function show(id) {
-		shown[id] = true;
-		if(id in segments) {
-			$.each(segments[id], function(index, value) {
-				value.setVisible(true);
-			});
-
-			return;
-		}
-
-		segments[id] = new Array();
-		$.each(line_data[id], function(index, value) {
-			var lat1 = value[0][0];
-			var lon1 = value[0][1];
-			var lat2 = value[1][0];
-			var lon2 = value[1][1];
-
-			var coordinates = [
-				new google.maps.LatLng(lat1, lon1),
-				new google.maps.LatLng(lat2, lon2)
-			];
-			var segment = new google.maps.Polyline({
-				path: coordinates,
-				strokeColor: '#FF0000',
-				strokeOpacity: 1.0,
-				strokeWeight: 2
-			});
-
-			segment.setMap(googleMap);
-			segments[id].push(segment);
-		});
-	}
-
-	function toggle(id) {
-		if(!(id in line_data)) {
-			$.ajax({
-				url: 'json.php?line='+id,
-				dataType: 'json',
-				success: function(data, text, xhr) {
-					line_data[id] = data;
-					show_hide(id);
-				}
-			});
-		}
-		else {
-			show_hide(id);
-		}
-	}
-	// -->
-	</script>
+	<script type="text/javascript" src="js/main.js"></script>
 </head>
 <body onload="initialize();">
 	<div id="options_pane" style="width: 20%; height: 100%; position: absolute; overflow: scroll;">
--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,7 @@
 var googleMap;
+var line_data = new Array();
+var segments = new Array();
+var shown = new Array();
 
 function initialize() {
 	var mapOptions = {
@@ -8,5 +11,70 @@ function initialize() {
 		streetViewControl: false
 	};
 	googleMap = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+}
+
+function show_hide(id) {
+	if((id in shown) && shown[id]) {
+		hide(id);
+	}
+	else {
+		show(id);
+	}
+}
+
+function hide(id) {
+	$.each(segments[id], function(index, value) {
+		value.setVisible(false);
+	});
+	shown[id] = false;
+}
+
+function show(id) {
+	shown[id] = true;
+	if(id in segments) {
+		$.each(segments[id], function(index, value) {
+			value.setVisible(true);
+		});
+
+		return;
+	}
+
+	segments[id] = new Array();
+	$.each(line_data[id], function(index, value) {
+		var lat1 = value[0][0];
+		var lon1 = value[0][1];
+		var lat2 = value[1][0];
+		var lon2 = value[1][1];
+
+		var coordinates = [
+			new google.maps.LatLng(lat1, lon1),
+			new google.maps.LatLng(lat2, lon2)
+		];
+		var segment = new google.maps.Polyline({
+			path: coordinates,
+			strokeColor: '#FF0000',
+			strokeOpacity: 1.0,
+			strokeWeight: 2
+		});
+
+		segment.setMap(googleMap);
+		segments[id].push(segment);
+	});
+}
+
+function toggle(id) {
+	if(!(id in line_data)) {
+		$.ajax({
+			url: 'json.php?line='+id,
+			dataType: 'json',
+			success: function(data, text, xhr) {
+				line_data[id] = data;
+				show_hide(id);
+			}
+		});
+	}
+	else {
+		show_hide(id);
+	}
 }
 
