04610628be7782ce57bb1cb27644aff65dbb81d4
paulchen <paulchen@rueckgr.at>
2013-09-01 21:19:56 +0200

    TODO added

--- web/json.php
+++ web/json.php
@@ -2,111 +2,119 @@
 
 require_once(dirname(__FILE__) . '/../lib/common.php');
 
-if(!isset($_REQUEST['line'])) {
+if(!isset($_REQUEST['lines'])) {
 	die();
 }
 
-$line = $_REQUEST['line'];
-if(!preg_match('/^[0-9]+$/', $line)) {
-	// TODO HTTP response: illegal request
-	die();
+$line_ids = explode(',', $_REQUEST['lines']);
+$placeholder_array = array();
+foreach($line_ids as $line_id) {
+	if(!preg_match('/^[0-9]+$/', $line_id)) {
+		// TODO HTTP response: illegal request
+		die();
+	}
+	$placeholder_array[] = '?';
 }
 
-$data = db_query('SELECT type FROM line WHERE id = ?', array($line));
-if(count($data) != 1) {
+$placeholders = implode(', ', $placeholder_array);
+
+$data = db_query("SELECT l.id id, COALESCE(c.color, t.color) color, t.line_thickness
+		FROM line l
+			LEFT JOIN line_color c ON (l.id = c.line)
+			JOIN line_type t ON (l.type = t.id)
+		WHERE l.deleted = 0
+			AND l.id IN ($placeholders)", $line_ids);
+if(count($data) != count($line_ids)) {
 	// TODO HTTP response: not found
 	die();
 }
-$type = $data[0]['type'];
-
-$data = db_query('SELECT color, line_thickness FROM line_type WHERE id = ?', array($type));
-$color = $data[0]['color'];
-$line_thickness = $data[0]['line_thickness'];
-
-$data = db_query('SELECT color FROM line_color WHERE line = ?', array($line));
-if(count($data) == 1) {
-	$color = $data[0]['color'];
-}
-
-$data = db_query('SELECT sp1.lat lat1, sp1.lon lon1, sp2.lat lat2, sp2.lon lon2
-		FROM line l
-			JOIN line_segment ls ON (l.id = ls.line)
-			JOIN segment s ON (ls.segment = s.id)
-			JOIN segment_point sp1 ON (s.point1 = sp1.id)
-			JOIN segment_point sp2 ON (s.point2 = sp2.id)
-		WHERE l.id = ?', array($line));
-$segments = array();
+$line_data = array();
 foreach($data as $row) {
-	$segments[] = array(array($row['lat1'], $row['lon1']), array($row['lat2'], $row['lon2']));
+	$line_data[$row['id']] = $row;
 }
-unset($data);
 
-$changed = true;
-while($changed) {
-	$changed = false;
+$result = array();
+foreach($line_ids as $line_id) {
+	$data = db_query('SELECT sp1.lat lat1, sp1.lon lon1, sp2.lat lat2, sp2.lon lon2
+			FROM line l
+				JOIN line_segment ls ON (l.id = ls.line)
+				JOIN segment s ON (ls.segment = s.id)
+				JOIN segment_point sp1 ON (s.point1 = sp1.id)
+				JOIN segment_point sp2 ON (s.point2 = sp2.id)
+			WHERE l.id = ?', array($line_id));
+	$segments = array();
+	foreach($data as $row) {
+		$segments[] = array(array($row['lat1'], $row['lon1']), array($row['lat2'], $row['lon2']));
+	}
+	unset($data);
 
-	for($a=0; $a<count($segments); $a++) {
-		for($b=0; $b<count($segments); $b++) {
-			if($a == $b) {
-				continue;
-			}
+	$changed = true;
+	while($changed) {
+		$changed = false;
 
-			if($segments[$a][count($segments[$a])-1][0] == $segments[$b][0][0] && $segments[$a][count($segments[$a])-1][1] == $segments[$b][0][1]) {
-				for($c=1; $c<count($segments[$b]); $c++) {
-					$segments[$a][] = $segments[$b][$c];
+		for($a=0; $a<count($segments); $a++) {
+			for($b=0; $b<count($segments); $b++) {
+				if($a == $b) {
+					continue;
 				}
 
-				unset($segments[$b]);
-				$segments = array_values($segments);
-				$changed = true;
-				continue 3;
+				if($segments[$a][count($segments[$a])-1][0] == $segments[$b][0][0] && $segments[$a][count($segments[$a])-1][1] == $segments[$b][0][1]) {
+					for($c=1; $c<count($segments[$b]); $c++) {
+						$segments[$a][] = $segments[$b][$c];
+					}
+
+					unset($segments[$b]);
+					$segments = array_values($segments);
+					$changed = true;
+					continue 3;
+				}
 			}
 		}
 	}
-}
 
-$data = db_query('SELECT s.id id, s.name name, p.direction direction, p.pos pos, p.lat, p.lon
-	FROM wl_platform p
-		JOIN station s ON (p.station = s.id)
-	WHERE p.line = ?
-	ORDER BY p.direction ASC, p.pos ASC', array($line));
-$stations = array();
-$known_station_ids = array();
-foreach($data as $row) {
-	$station_id = $row['id'];
-	if(in_array($station_id, $known_station_ids)) {
-		continue;
+	$data = db_query('SELECT s.id id, s.name name, p.direction direction, p.pos pos, p.lat, p.lon
+		FROM wl_platform p
+			JOIN station s ON (p.station = s.id)
+		WHERE p.line = ?
+		ORDER BY p.direction ASC, p.pos ASC', array($line_id));
+	$stations = array();
+	$known_station_ids = array();
+	foreach($data as $row) {
+		$station_id = $row['id'];
+		if(in_array($station_id, $known_station_ids)) {
+			continue;
+		}
+		$known_station_ids[] = $station_id;
+
+		unset($row['direction']);
+		unset($row['pos']);
+		$stations[] = $row;
 	}
-	$known_station_ids[] = $station_id;
 
-	unset($row['direction']);
-	unset($row['pos']);
-	$stations[] = $row;
-}
-
-$directions_difference = false;
-for($a=0; $a<count($data)/2; $a++) {
-	if($data[$a] != $data[count($data)-1-$a]) {
-		$directions_difference = true;
+	$directions_difference = false;
+	for($a=0; $a<count($data)/2; $a++) {
+		if($data[$a] != $data[count($data)-1-$a]) {
+			$directions_difference = true;
+		}
 	}
-}
 
-$routes = array(array());
-if($directions_difference) {
-	$routes[] = array();
-}
-foreach($data as $row) {
-	$direction = $row['direction'];
-	unset($row['direction']);
-	if($direction == 1) {
-		$routes[0][] = $row;
+	$routes = array(array());
+	if($directions_difference) {
+		$routes[] = array();
 	}
-	else if($directions_difference) {
-		$routes[1][] = $row;
+	foreach($data as $row) {
+		$direction = $row['direction'];
+		unset($row['direction']);
+		if($direction == 1) {
+			$routes[0][] = $row;
+		}
+		else if($directions_difference) {
+			$routes[1][] = $row;
+		}
 	}
-}
 
-$result = array('segments' => $segments, 'color' => $color, 'line_thickness' => $line_thickness, 'stations' => $stations, 'routes' => $routes);
+	$result[] = array('line' => $line_id, 'segments' => $segments, 'color' => $color, 'line_thickness' => $line_thickness, 'stations' => $stations, 'routes' => $routes);
+}
 
 header('Content-Type: application/json');
 echo json_encode($result);
