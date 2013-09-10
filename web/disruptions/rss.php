18bc6c7850b6092df98178dd5794d1ac8a053d97
paulchen <paulchen@rueckgr.at>
2013-09-10 16:31:33 +0200

    update

--- web/disruptions/rss.php
+++ web/disruptions/rss.php
@@ -3,9 +3,14 @@ require_once(dirname(__FILE__) . '/../../lib/common.php');
 
 $disruptions = get_disruptions();
 $feed_date = 0;
-foreach($disruptions as $disruption) {
+foreach($disruptions as &$disruption) {
 	$feed_date = max($feed_date, $disruption['start_time']);
+
+	if(count($disruption['lines']) == 1) {
+		$disruption['title'] = str_replace($disruption['lines'][0] . ' ', '', $disruption['title']);
+	}
 }
+unset($disruption);
 
 $link = "https://{$_SERVER['SERVER_NAME']}" . dirname($_SERVER['PHP_SELF']) . '/';
 
