0bc57a110e46a91b8c62a9af5185306201105ac3
paulchen <paulchen@rueckgr.at>
2013-09-11 15:35:42 +0200

    update

--- templates/disruptions_html.php
+++ templates/disruptions_html.php
@@ -2,11 +2,11 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-	<title>Wiener Linien -- Aktuelle Störungen</title>
+	<title>Wiener Linien -- <?php if(isset($_REQUEST['id'])): ?>Störungsdetails<?php else: ?>Aktuelle Störungen<?php endif; ?></title>
 	<link rel="alternate" type="application/rss+xml" title="Wiener Linien -- Aktuelle Störungen -- RSS-Feed"  href="rss.xml" />
 </head>
 <body>
-	<h1>Aktuelle Störungen</h1>
+	<h1><?php if(isset($_REQUEST['id'])): ?>Störungsdetails<?php else: ?>Aktuelle Störungen<?php endif; ?></h1>
 	<?php foreach($disruptions as $disruption): ?>
 		<h2><a href="?id=<?php echo $disruption['id'] ?>">[<?php echo htmlentities($disruption['category'], ENT_QUOTES, 'UTF-8') ?>] <?php echo htmlentities($disruption['title'], ENT_QUOTES, 'UTF-8') ?></a></h2>
 		<?php require('disruption_description.php'); ?>
