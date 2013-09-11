ab550f97e6cdfd46c0cce95b647a0a6e979844c4
paulchen <paulchen@rueckgr.at>
2013-09-12 00:41:36 +0200

    update

--- templates/station.php
+++ templates/station.php
@@ -15,7 +15,7 @@
 	<h1><?php echo htmlentities($station_name, ENT_QUOTES, 'UTF-8') ?></h1>
 	<?php foreach($platforms as $platform): ?>
 	<?php if(!isset($previous_line) || $previous_line != $platform['line_id']): ?><h2><?php echo htmlentities($platform['line_name'], ENT_QUOTES, 'UTF-8') ?></h2><?php endif; ?>
-	<div id="rbl_<?php echo $platform['rbl'] ?>"></div>
+	<div class="rbl_<?php echo $platform['rbl'] ?>"></div>
 	<?php $previous_line = $platform['line_id']; endforeach; ?>
 </body>
 </html>
--- web/js/rbl.js
+++ web/js/rbl.js
@@ -7,7 +7,34 @@ function update_rbls() {
 		url: 'rbls.php?ids=' + rbls.join(','),
 		dataType: 'json',
 		success: function(data, text, xhr) {
-			// TODO
+			$.each(rbls, function(index, rbl) {
+
+				if(rbl in data) {
+					var content = '<table class="rbl_info">';
+
+					$.each(data[rbl], function(index2, row) {
+						var line = row['name'];
+						var towards = row['towards'];
+
+						if(row['departures']['departure'].length > 0) {
+							$.each(row['departures']['departure'], function(index3, departure) {
+								var time = departure['departureTime']['countdown'];
+								if(time == undefined) {
+									time = '&ndash;';
+								}
+								content += '<tr><td>' + line + '</td><td>' + towards + '</td><td>' + time + '</td></tr>';
+							});
+						}
+						else {
+							content += '<tr><td>' + line + '</td><td>' + towards + '</td><td>&ndash;</td></tr>';
+						}
+					});
+
+					content += '</table>';
+
+					$('.rbl_' + rbl).html(content);
+				}
+			});
 		},
 		complete: function(xhr, text) {
 			window.setTimeout('update_rbls();', 30000);
