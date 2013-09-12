f5298ef2afebe52f5bf42fb75d1c2595c7d525b7
paulchen <paulchen@rueckgr.at>
2013-09-12 19:35:24 +0200

    update

--- templates/station.php
+++ templates/station.php
@@ -14,6 +14,8 @@
 </head>
 <body>
 	<h1><?php echo htmlentities($station_name, ENT_QUOTES, 'UTF-8') ?> &ndash; Nächste Abfahrten</h1>
+	<div id="last_update"></div>
+	<div id="next_update"></div>
 	<?php foreach($platforms as $platform): ?>
 	<?php if(!isset($previous_lines) || $previous_lines != $platform['line_ids']): ?><h2><?php echo htmlentities(implode(', ', $platform['line_names']), ENT_QUOTES, 'UTF-8') ?></h2><?php endif; ?>
 	<div>Bahnsteig <?php echo htmlentities($platform['platform'], ENT_QUOTES, 'UTF-8') ?>:</div>
--- web/js/rbl.js
+++ web/js/rbl.js
@@ -2,7 +2,21 @@ $(document).ready(function() {
 	update_rbls();
 });
 
+function update_countdown(seconds) {
+	if(seconds == 0) {
+		update_rbls();
+	}
+	else {
+		// TODO show updating image
+		$('#next_update').html('Nächste Aktualisierung in ' + seconds + ' Sekunden');
+		window.setTimeout('update_countdown(' + (seconds-1) + ');', 1000);
+	}
+
+}
+
 function update_rbls() {
+	$('#next_update').html('Aktualisierung läuft...');
+
 	$.ajax({
 		url: 'rbls.php?ids=' + rbls.join(','),
 		dataType: 'json',
@@ -23,10 +37,12 @@ function update_rbls() {
 
 					$('#rbl_' + rbl).html(content);
 				}
+
+				// TODO update last_update
 			});
 		},
 		complete: function(xhr, text) {
-			window.setTimeout('update_rbls();', 30000);
+			update_countdown(30);
 		}
 	});
 }
