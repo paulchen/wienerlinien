cb8696ea5475d9ba4953ae28a69dab981c55d5bd
paulchen <paulchen@rueckgr.at>
2013-09-13 00:32:58 +0200

    update

--- templates/map.php
+++ templates/map.php
@@ -18,13 +18,13 @@ function show_lines($lines) {
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 	<title>Wiener Linien</title>
-	<link rel="stylesheet" type="text/css" href="../css/map.css" />
+	<link rel="stylesheet" type="text/css" href="../css/main.css" />
 	<script type="text/javascript" src="//maps.googleapis.com/maps/api/js?key=AIzaSyAj4Id5jnWqbSeAm0YcSoep75ujK2h8T70&amp;sensor=false"></script>
 	<script type="text/javascript" src="../js/jquery.min.js"></script>
 	<script type="text/javascript" src="../js/jquery.fancybox.js"></script>
 	<link rel="stylesheet" type="text/css" href="../css/jquery.fancybox.css" media="screen" />
 
-	<script type="text/javascript" src="../js/main.js"></script>
+	<script type="text/javascript" src="../js/map.js"></script>
 	<script type="text/javascript">
 	<!--
 
@@ -48,10 +48,12 @@ $(document).ready(function() {
 	</script>
 </head>
 <body onload="initialize();" class="map">
-	<div id="options_pane" style="width: 20%; height: 100%; position: absolute; overflow-y: scroll; overflow-x: hidden;">
-		<?php show_lines($lines); ?>
-		<hr />
-		<?php require('footer.php'); ?>
+	<div style="width: 20%; height: 100%; position: absolute; overflow-y: scroll; overflow-x: hidden;">
+		<div id="options_pane">
+			<?php show_lines($lines); ?>
+			<hr />
+			<?php require('footer.php'); ?>
+		</div>
 	</div>
 	<div id="map_canvas" style="position: absolute; width:80%; left: 20%; height:100%"></div>
 </body>
