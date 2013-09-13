d4a5725030d85cabef5e416691662ab3a1c5790c
paulchen <paulchen@rueckgr.at>
2013-09-14 01:41:36 +0200

    update

--- templates/disruptions_html.php
+++ templates/disruptions_html.php
@@ -5,15 +5,43 @@
 	<title>Wiener Linien -- <?php if(isset($_REQUEST['id'])): ?>Störungsdetails<?php else: ?>Aktuelle Störungen<?php endif; ?></title>
 	<link rel="alternate" type="application/rss+xml" title="Wiener Linien -- Aktuelle Störungen -- RSS-Feed"  href="rss.xml" />
 	<link rel="stylesheet" type="text/css" href="../css/main.css" />
+	<style type="text/css">
+	a.show_link, a.hide_link { display: none; }
+	div.description { padding-top: 1em; }
+	</style>
+	<script type="text/javascript" src="../js/jquery.min.js"></script>
+	<script type="text/javascript">
+	<!--
+	$(document).ready(function() {
+		$('.description').hide();
+		$('.show_link').show();
+	});
+
+	function show(id) {
+		$('#show_link_' + id).hide();
+		$('#hide_link_' + id).show();
+		$('#description_' + id).show('fast');
+	}
+
+	function hide(id) {
+		$('#show_link_' + id).show();
+		$('#hide_link_' + id).hide();
+		$('#description_' + id).hide('fast');
+	}
+	// ->
+	</script>
 </head>
 <body>
 	<h1><?php if(isset($_REQUEST['id'])): ?>Störungsdetails<?php else: ?>Aktuelle Störungen<?php endif; ?></h1>
 	<?php foreach($disruptions as $disruption): ?>
 		<h2><a href="?id=<?php echo $disruption['id'] ?>">[<?php echo htmlentities($disruption['category'], ENT_QUOTES, 'UTF-8') ?>] <?php echo htmlentities($disruption['title'], ENT_QUOTES, 'UTF-8') ?></a></h2>
-		<?php if($disruption['deleted']): ?>
-			<div style="color: red; font-weight: bold; margin-bottom: 1em;">Diese Störung ist bereits zu Ende.</div>
-		<?php endif; ?>
-		<?php require('disruption_description.php'); ?>
+		<a href="javascript:show(<?php echo $disruption['id'] ?>);" class="show_link" id="show_link_<?php echo $disruption['id'] ?>">Anzeigen</a><a href="javascript:hide(<?php echo $disruption['id'] ?>);" class="hide_link" id="hide_link_<?php echo $disruption['id'] ?>">Verbergen</a>
+		<div class="description" id="description_<?php echo $disruption['id'] ?>">
+			<?php if($disruption['deleted']): ?>
+				<div style="color: red; font-weight: bold; margin-bottom: 1em;">Diese Störung ist bereits zu Ende.</div>
+			<?php endif; ?>
+			<?php require('disruption_description.php'); ?>
+		</div>
 		<hr />
 	<?php endforeach; ?>
 	<?php if(isset($_REQUEST['id'])): ?>
