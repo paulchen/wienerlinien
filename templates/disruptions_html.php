cdc33933e571fb8e81a69b11c891171f036ee5d2
paulchen <paulchen@rueckgr.at>
2013-09-14 17:02:03 +0200

    update

--- templates/disruptions_html.php
+++ templates/disruptions_html.php
@@ -2,7 +2,7 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-	<title>Wiener Linien -- <?php if(isset($_REQUEST['id'])): ?>Störungsdetails<?php else: ?>Aktuelle Störungen<?php endif; ?></title>
+	<title>Wiener Linien -- <?php if(isset($_REQUEST['id'])): ?>Störungsdetails<?php else: ?>Störungen<?php endif; ?></title>
 	<link rel="alternate" type="application/rss+xml" title="Wiener Linien -- <?php if(isset($_REQUEST['archive']) && $_REQUEST['archive'] == 1): ?>Alle<?php else: ?>Aktuelle<? endif; ?> Störungen -- RSS-Feed"  href="rss.xml" />
 	<link rel="stylesheet" type="text/css" href="../css/main.css" />
 	<style type="text/css">
@@ -44,6 +44,14 @@
 			Störungen
 		<?php endif; ?>
 	</h1>
+	<div>
+		<?php if(isset($_REQUEST['id']) || (isset($_REQUEST['archive']) && $_REQUEST['archive'] == 1)): ?>
+			<a href="?">Aktuelle Störungen</a>
+		<?php endif; ?>
+		<?php if(isset($_REQUEST['id']) || !isset($_REQUEST['archive']) || $_REQUEST['archive'] != 1): ?>
+			<a href="?archive=1">Alle Störungen</a>
+		<?php endif; ?>
+	</div>
 	<?php foreach($disruptions as $disruption): ?>
 		<h2><a href="?id=<?php echo $disruption['id'] ?>"><?php if(count($disruption['lines']) > 0): echo implode('/', $disruption['lines']) . ': '; endif; echo '[' . htmlentities($disruption['category'], ENT_QUOTES, 'UTF-8') ?>] <?php echo htmlentities($disruption['title'], ENT_QUOTES, 'UTF-8') ?></a></h2>
 		<a href="javascript:show(<?php echo $disruption['id'] ?>);" class="show_link" id="show_link_<?php echo $disruption['id'] ?>">Anzeigen</a><a href="javascript:hide(<?php echo $disruption['id'] ?>);" class="hide_link" id="hide_link_<?php echo $disruption['id'] ?>">Verbergen</a>
--- templates/index.php
+++ templates/index.php
@@ -10,8 +10,9 @@
 	<div>Projekte auf Basis der von der Stadt Wien und den Wiener Linien zur Verfügung gestellten Daten.</div>
 	<div>
 		<ul>
-			<li><a href="disruptions">Aktuelle Störungen</a></li>
+			<li><a href="disruptions/">Aktuelle Störungen</a></li>
 			<li><a href="disruptions/rss.php">Aktuelle Störungen -- RSS-Feed</a></li>
+			<li><a href="disruptions/?archive=1">Störungsarchiv</a></li>
 			<li><a href="map">Karte</a></li>
 		</ul>
 	</div>
