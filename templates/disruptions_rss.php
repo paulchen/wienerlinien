26090e553c3585e06f5b0c4e1e73872d287a01ac
paulchen <paulchen@rueckgr.at>
2013-09-20 21:12:20 +0200

    bugfix

--- templates/disruptions_rss.php
+++ templates/disruptions_rss.php
@@ -9,7 +9,7 @@
   <pubDate><?php echo date(DateTime::RSS, $feed_date) ?></pubDate>
   <?php foreach($disruptions as $disruption): ?>
     <item>
-      <title><![CDATA[<?php if(count($disruption['lines']) > 0): echo implode('/', $disruption['lines']) . ': '; endif; echo htmlspecialchars('[' . $disruption['category'] . '] ' . str_replace("\n", " ", $disruption['title']), ENT_QUOTES, 'UTF-8'); ?>]]></title>
+      <title><![CDATA[<?php if(count($disruption['lines']) > 0): echo implode('/', $disruption['lines']) . ': '; endif; echo htmlspecialchars('[' . $disruption['category'] . '] ' . str_replace("\n", " ", $disruption['title']), ENT_COMPAT, 'UTF-8'); ?>]]></title>
       <link><?php echo "$link?id={$disruption['id']}" ?></link>
       <guid><?php echo "$link?id={$disruption['id']}" ?></guid>
       <pubDate><?php echo date(DateTime::RSS, $disruption['start_time']) ?></pubDate>
