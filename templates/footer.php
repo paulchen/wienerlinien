60c40219adaf746bde13ef060286d5c3c9bb32ea
paulchen <paulchen@rueckgr.at>
2018-12-22 00:28:00 +0100

    updated source links

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,3 @@
 Seiteninhaber: <a href="mailto:paulchen@rueckgr.at">Paul Staroch</a><br />
-<br />Datenquelle: Stadt Wien - <a href="http://data.wien.gv.at">data.wien.gv.at</a>, <a href="https://open.wien.at/site/">open.wien.at</a>
+<br />Datenquelle: <a href="https://data.wien.gv.at">Datenauftritt - Stadt Wien</a>, <a href="https://digitales.wien.gv.at/site/">DigitalesWien</a>
 
