0aae2b7b9c47dcdc309b2ef6726ae46b30032c89
paulchen <paulchen@rueckgr.at>
2020-02-21 23:11:26 +0100

    don't hardcode id of municipality

--- web/map/stations.php
+++ web/map/stations.php
@@ -2,16 +2,16 @@
 
 require_once(dirname(__FILE__) . '/../../lib/common.php');
 
-$query = 'SELECT s.id, s.name, ROUND(s.wl_lat, 4) lat, ROUND(s.wl_lon, 4) lon, COUNT(DISTINCT l.id) line_count
+$query = "SELECT s.id, s.name, ROUND(s.wl_lat, 4) lat, ROUND(s.wl_lon, 4) lon, COUNT(DISTINCT l.id) line_count
 	FROM `station` s
 		JOIN wl_platform p ON (s.id = p.station)
 		JOIN line l ON (p.line = l.id)
 	WHERE s.deleted = 0
 		AND p.deleted = 0
 		AND l.deleted = 0
-		AND municipality = 1
+		AND municipality IN (SELECT id FROM municipality WHERE name = 'Wien')
 	GROUP BY s.id, s.name, s.wl_lat, s.wl_lon
-	ORDER BY line_count DESC, s.name ASC';
+	ORDER BY line_count DESC, s.name ASC";
 $data = db_query($query);
 
 header('Content-Type: application/json');
