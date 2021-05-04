6d035131842a99936a5c8e6c27f89188d0a3ba93
paulchen <paulchen@rueckgr.at>
2021-05-04 17:35:07 +0200

    trim RBL ids and ignore blank ones

--- web/map/rbls.php
+++ web/map/rbls.php
@@ -5,7 +5,7 @@ require_once(dirname(__FILE__) . '/../../lib/common.php');
 if(!isset($_REQUEST['ids'])) {
 	die();
 }
-$ids = array_unique(explode(',', $_REQUEST['ids']));
+$ids = array_filter(array_unique(array_map("trim", explode(',', $_REQUEST['ids']))), function($id) { return $id != ''; });
 $placeholders = array();
 foreach($ids as $id) {
 	$placeholders[] = '?';
