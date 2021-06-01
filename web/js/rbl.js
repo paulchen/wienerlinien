1690e856a7438852552ce5c9da932875862057e8
paulchen <paulchen@rueckgr.at>
2021-06-01 18:03:22 +0200

    fix problems with stations not having any RBL number

--- web/js/rbl.js
+++ web/js/rbl.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#last_update').html('Letzte Aktivierung: nie');
+	$('#last_update').html('Letzte Aktualisierung: nie');
 	update_countdown(0);
 });
 
@@ -26,62 +26,71 @@ function force_refresh() {
 	update_countdown(0);
 }
 
+function process_result(data) {
+	$.each(rbls, function(index, rbl) {
+		if(rbl in data) {
+			var content = '<table class="rbl_info">';
+
+			$.each(data[rbl], function(index2, row) {
+				var line = row['line'];
+				var towards = row['towards'];
+				var time = row['time'];
+				var line_link = 'line.htm?id=' + row['line_id'];
+				var barrier_free = '<span style="visibility: hidden;">&#x267f;</span>';
+				if(row['folding_ramp']) {
+					barrier_free = '&#x2581;';
+				}
+				else if(row['barrier_free']) {
+					barrier_free = '&#x267f;';
+				}
+
+				content += '<tr>';
+				if(line == null) {
+					content += '<td></td>';
+				}
+				else if(row['line_id'] == null) {
+					content += '<td>' + line + '</td>';
+				}
+				else {
+					content += '<td><a href="' + line_link + '">' + line + '</a></td>';
+				}
+				if(row['towards_id'] == null) {
+					content += '<td>' + towards + '</td>';
+				}
+				else {
+					content += '<td><a href="station.htm?id=' + row['towards_id'] + '">' + towards + '</a></td>';
+				}
+				if(time == null) {
+					content += '<td></td><td></td>';
+				}
+				else {
+					content += '<td>' +  time + '</td><td>' + barrier_free + '</td>';
+				}
+				content += '</tr>';
+			});
+
+			content += '</table>';
+		}
+		else {
+			content = 'Derzeit sind für diesen Bahnsteig keine Abfahrtsinformationen verfügbar.';
+		}
+
+		$('#rbl_' + rbl).html(content);
+	});
+	$('#last_update').html('Letzte Aktualisierung: ' + (new Date()).toString('dd.MM.yyyy HH:mm:ss'));
+}
+
 function update_rbls() {
+	if(rbls.length == 0) {
+		process_result([]);
+		update_countdown(30);
+		return;
+	}
 	$.ajax({
 		url: 'rbls.php?ids=' + rbls.join(','),
 		dataType: 'json',
 		success: function(data, text, xhr) {
-			$.each(rbls, function(index, rbl) {
-				if(rbl in data) {
-					var content = '<table class="rbl_info">';
-
-					$.each(data[rbl], function(index2, row) {
-						var line = row['line'];
-						var towards = row['towards'];
-						var time = row['time'];
-						var line_link = 'line.htm?id=' + row['line_id'];
-						var barrier_free = '<span style="visibility: hidden;">&#x267f;</span>';
-						if(row['folding_ramp']) {
-							barrier_free = '&#x2581;';
-						}
-						else if(row['barrier_free']) {
-							barrier_free = '&#x267f;';
-						}
-
-						content += '<tr>';
-						if(line == null) {
-						       	content += '<td></td>';
-						}
-						else if(row['line_id'] == null) {
-							content += '<td>' + line + '</td>';
-						}
-						else {
-							content += '<td><a href="' + line_link + '">' + line + '</a></td>';
-						}
-						if(row['towards_id'] == null) {
-							content += '<td>' + towards + '</td>';
-						}
-						else {
-							content += '<td><a href="station.htm?id=' + row['towards_id'] + '">' + towards + '</a></td>';
-						}
-						if(time == null) {
-							content += '<td></td><td></td>';
-						}
-						else {
-							content += '<td>' +  time + '</td><td>' + barrier_free + '</td>';
-						}
-						content += '</tr>';
-					});
-
-					content += '</table>';
-				}
-				else {
-					content = 'Derzeit sind für diesen Bahnsteig keine Abfahrtsinformationen verfügbar.';
-				}
-
-				$('#rbl_' + rbl).html(content);
-				$('#last_update').html('Letzte Aktualisierung: ' + (new Date()).toString('dd.MM.yyyy HH:mm:ss'));
-			});
+			process_result(data);
 		},
 		complete: function(xhr, text) {
 			update_countdown(30);
--- web/map/rbls.php
+++ web/map/rbls.php
@@ -6,15 +6,17 @@ if(!isset($_REQUEST['ids'])) {
 	die();
 }
 $ids = array_filter(array_unique(array_map("trim", explode(',', $_REQUEST['ids']))), function($id) { return $id != ''; });
-$placeholders = array();
-foreach($ids as $id) {
-	$placeholders[] = '?';
-}
-$placeholder_string = implode(',', $placeholders);
-$data = db_query("SELECT DISTINCT(rbl) FROM wl_platform WHERE rbl in ($placeholder_string) AND deleted = 0", $ids);
-if(count($data) != count($ids)) {
-	// TODO
-	die();
+if(count($ids) > 0) {
+	$placeholders = array();
+	foreach($ids as $id) {
+		$placeholders[] = '?';
+	}
+	$placeholder_string = implode(',', $placeholders);
+	$data = db_query("SELECT DISTINCT(rbl) FROM wl_platform WHERE rbl in ($placeholder_string) AND deleted = 0", $ids);
+	if(count($data) != count($ids)) {
+		// TODO
+		die();
+	}
 }
 
 echo json_encode(fetch_rbls($ids));
